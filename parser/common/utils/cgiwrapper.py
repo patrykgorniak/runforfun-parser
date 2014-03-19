5aad30c82c52a805acc611d091600986cb222821
Patryk Gorniak <patryk.lukasz.gorniak@gmail.com>
2014-03-19 22:11:51 +0100

    Utils: 	Minor refactoring, added charset=utf-8 	for HTTP header

--- parser/common/utils/cgiwrapper.py
+++ parser/common/utils/cgiwrapper.py
@@ -1,8 +1,10 @@
 STATUS_OK = 'Status: 200 OK'
 STATUS_UNAUTHORIZED = 'Status: 403 Unauthorized'
 
-CONTENT_HTML = "Content-type: text/html"
-CONTENT_PLAIN = "Content-type: text/plain"
+CONTENT_HTML = "Content-type: text/html; charset=UTF-8"
+CONTENT_PLAIN = "Content-type: text/plain; charset=UTF-8"
+CONTENT_JSON = "Content-type: application/json;  charset=UTF-8"
+
 
 
 def publish(content=None):
@@ -10,7 +12,7 @@ def publish(content=None):
         content = "Error occured!"
 
     print(STATUS_OK)
-    print(CONTENT_HTML)
-    print("Length: {}", len(content))
+    print(CONTENT_PLAIN)
+    print("Length: {}".format(len(content)))
     print()
     print(content)
