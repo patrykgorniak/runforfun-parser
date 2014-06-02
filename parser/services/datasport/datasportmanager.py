334bd034af29da6a3cde302e9fe0c60f21b043d2
Patryk Gorniak <patryk.lukasz.gorniak@gmail.com>
2014-06-03 00:28:24 +0200

    General: code refactoring

--- parser/services/datasport/datasportmanager.py
+++ parser/services/datasport/datasportmanager.py
@@ -15,13 +15,15 @@ def get_events(args=None):
     results = eventlist.unpack_events(content, args)
     return json.dumps({'Status': res.reason, 'Data': results}, indent=4)
 
+
 def myaccount(args):
     params = {}
     user_data = {}
     logger.debug("My account request.")
     ret, headers = authorization.login(args)
-    if ret==-1:
-        return json.dumps( {'Status': "FAILED"}, indent = 4)
+
+    if ret == -1:
+        return json.dumps({'Status': "FAILED"}, indent=4)
     else:
         params['id'] = ret
         params['los'] = authorization.los()
@@ -29,5 +31,5 @@ def myaccount(args):
 
         res, cont = httpmanager.httprequest(COMMON_DATA['CHANGE_USER_DATA']['URL'], COMMON_DATA['CHANGE_USER_DATA']['LOGIN_NEEDED'], params, 'GET', headers)
         user_data.update(accountmanager.get_user_data(cont))
-        jsonf = json.dumps({'Status':res.reason, 'Cookie': headers['Cookie'], 'Data': user_data }, indent = 4)
+        jsonf = json.dumps({'Status': res.reason, 'Cookie': headers['Cookie'], 'Data': user_data}, indent=4)
         return jsonf
