0440d5e30fbe110619187642d9e89f7cc6751acb
Patryk Gorniak <patryk.lukasz.gorniak@gmail.com>
2014-06-10 00:55:46 +0200

    Common: HttpResponse refactoring

--- parser/common/utils/httpresponse.py
+++ parser/common/utils/httpresponse.py
@@ -1,9 +1,17 @@
 import json
+from enum import Enum
+
+class HttpResponseStatus:
+    FAILED = "FAILED"
+    OK = "OK"
 
 class HttpResponse:
     def __init__(self):
        self.indent = 4
-       self.container = {}
+       self.container = {"Status": HttpResponseStatus.FAILED }
+
+    def set_status(self, value):
+        self.container["Status"] = value
 
     def add_node(self, key, value):
         self.container[key] = value
@@ -11,10 +19,11 @@ class HttpResponse:
     def __str__(self):
         return json.dumps(self.container, indent = self.indent)
 
-    @staticmethod
-    def error():
-        return json.dumps({'Status': "FAILED"}, indent = 4)
-
-    @staticmethod
-    def error(reason):
-        return json.dumps({'Status': "FAILED",  'Data': '{0} does not exist.'.format(reason)}, indent = 4)
+#    @staticmethod
+#    def error(*args):
+#        if len(args) == 0:
+#            return json.dumps({'Status': "FAILED"}, indent=4)
+#        elif len(args) == 1:
+#            return json.dumps({'Status': "FAILED",  'Data': '{0} does not exist!'.format(args[0])}, indent=4)
+#        else:
+#            pass
